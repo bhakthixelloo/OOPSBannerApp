3886c598408d5b5b0e5e3ddb6d00c4c4cc41bf36
hp <bm@8194@srmist.edu.in>
2026-03-02 21:48:36 +0530

    UC1:Initialized project and printed OOPS literal

---
+++ OOPSBannerApp.java
@@ -0,0 +1,7 @@
+
+public class OOPSBannerApp {
+    public static void main(String[] args) {
+        // UC1: Simple print statement to establish entry point
+        System.out.println("OOPS");
+    }
+} 
